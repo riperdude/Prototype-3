13b187b38a3c34c1eaa46c653aff801890fea46c
riperdude <146127014+riperdude@users.noreply.github.com>
2023-10-11 12:05:09 -0700

    Finished up to 3.1.5

---
+++ Sounds and Effects/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float JumpForce = 10f;
+    public float GravityModifier = 1f;
+
+    private Rigidbody _playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+     _playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= GravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _playerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        }
+    }
+}
