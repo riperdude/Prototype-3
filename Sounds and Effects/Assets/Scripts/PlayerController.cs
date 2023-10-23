5fd649efd65197eadbf8cac9c91686c5994ebc45
riperdude <146127014+riperdude@users.noreply.github.com>
2023-10-23 12:02:44 -0700

    Update 3.3

--- Sounds and Effects/Assets/Scripts/PlayerController.cs
+++ Sounds and Effects/Assets/Scripts/PlayerController.cs
@@ -14,14 +14,15 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     _playerRb = GetComponent<Rigidbody>();
+        _playerRb = GetComponent<Rigidbody>();
+        _playerAnim = GetComponent<Animator>();
         Physics.gravity *= GravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && IsOnGround)
+        if(Input.GetKeyDown(KeyCode.Space) && IsOnGround && gameOver == false)
         {
             _playerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
             IsOnGround = false;
@@ -40,6 +41,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Game Over!");
             gameOver = true;
+            _playerAnim.SetBool("Death_b", true);
+            _playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
